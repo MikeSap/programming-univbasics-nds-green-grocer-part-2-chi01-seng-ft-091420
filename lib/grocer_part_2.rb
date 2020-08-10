219bfc1869b8f9f3520820d44e6891f5e0c7f934
Mike Sapienza <mikesapaudio@gmail.com>
2020-08-10 18:22:16 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -5,7 +5,7 @@ def apply_coupons(cart, coupons)
       #binding.pry
       item_w_coupon = find_item_by_name_in_collection(disc[:item], cart)
            if  !!item_w_coupon && coupon[:num] <=  item_w_coupon[:count]
-             final_item = {:item => 'item_w_coupon[:item] W/ COUPON'
+             final_item = {:item => "#{item_w_coupon[:item]} W/COUPON", :price => coupon[:cost]/item_w_coupon
         coupon[:cost] = [:cost] / [:num]
     end
   end
