ee645b7feb0042d1f2dc2b1b8a61500078b69bb0
Mike Sapienza <mikesapaudio@gmail.com>
2020-08-10 22:22:13 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -28,16 +28,6 @@ def checkout(cart, coupons)
   apply_coupons(food, coupons)
   apply_clearance(food)
   food.each do |items|
-    
-  binding.pry
-end
-  # Consult README for inputs and outputs
-  #
-  # This method should call
-  # * consolidate_cart
-  # * apply_coupons
-  # * apply_clearance
-  #
-  # BEFORE it begins the work of calculating the total (or else you might have
-  # some irritated customers
+    total = total + (food[:price] * food[:count])
+  end
 end
